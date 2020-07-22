b8d823824b0797031639a1ffdb94c6a7e469e769
Jinx66666666 <59303283+Jinx66666666@users.noreply.github.com>
2020-07-22 17:46:39 +0800

    Update ConfigurationPropertiesController.java
    @ConfigurationProperties注解添加注释

--- jin_springboot/src/main/java/com/jin/controller/ConfigurationPropertiesController.java
+++ jin_springboot/src/main/java/com/jin/controller/ConfigurationPropertiesController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @ClassName ConfigurationPropertiesController
  * @program: Jin_SpringBoot
- * @Description: 使用注解@ConfigurationProperties映射
+ * @Description: 使用注解@ConfigurationProperties映射，将配置文件的每一个属性的值，映射到这个组件中，告诉SpringBoot将本类中所有属性和配置文件中相关的配置进行绑定
  * @author: HuangJing
  * @Date: 2020/04/19 0:03
  * @create: 2020-04-19 00:03
  * @version: V1.0
  */
 @Controller
-@ConfigurationProperties(prefix = "persion")
+@ConfigurationProperties(prefix = "persion")//配置文件中对象下的所有属性进行一一映射
 public class ConfigurationPropertiesController {
 
     private String name;
